153de1bbfd1f04ea47361cae456a85a7d9e8677c
Aergiaaa <125899328+Aergiaaa@users.noreply.github.com>
2025-02-16 20:17:44 +0700

    Adding some test helper and updating test, also adding clear func on store struct

--- store.go
+++ store.go
@@ -4,9 +4,9 @@ import (
 	"bytes"
 	"crypto/sha1"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
-	"io/fs"
 	"log"
 	"os"
 	"strings"
@@ -83,7 +83,11 @@ func (s *Store) Has(key string) bool {
 	pathKey := s.PathTransformFunc(key)
 	fullPathWithRoot := fmt.Sprintf("%s/%s", s.Root, pathKey.FullPath())
 	_, err := os.Stat(fullPathWithRoot)
-	return err != fs.ErrNotExist
+	return errors.Is(err, os.ErrNotExist)
+}
+
+func (s *Store) Clear() error {
+	return os.RemoveAll(s.Root)
 }
 
 func (s *Store) Delete(key string) error {
--- store_test.go
+++ store_test.go
@@ -10,23 +10,20 @@ import (
 func TestPathTransformFunc(t *testing.T) {
 	key := "myjpg"
 	pathKey := CASPathTransformFunc(key)
-	expectedOriginalKey := "717d5b5fe0e1a5f88216179791868b6ca042c155"
+	expectedFileName := "717d5b5fe0e1a5f88216179791868b6ca042c155"
 	expectedPathName := "717d5/b5fe0/e1a5f/88216/17979/1868b/6ca04/2c155"
 
 	if pathKey.PathName != expectedPathName {
 		t.Errorf("expected %s, got %s", expectedPathName, pathKey.PathName)
 	}
 
-	if pathKey.Filename != expectedOriginalKey {
-		t.Errorf("expected %s, got %s", expectedOriginalKey, pathKey.Filename)
+	if pathKey.Filename != expectedFileName {
+		t.Errorf("expected %s, got %s", expectedFileName, pathKey.Filename)
 	}
 }
 
 func TestStoreDelete(t *testing.T) {
-	cfg := StoreConfig{
-		PathTransformFunc: CASPathTransformFunc,
-	}
-	s := NewStore(cfg)
+	s := newStoreHelper()
 	key := "special"
 
 	data := []byte("some data")
@@ -40,36 +37,53 @@ func TestStoreDelete(t *testing.T) {
 }
 
 func TestStore(t *testing.T) {
+	s := newStoreHelper()
+	defer teardown(t, s)
+
+	for i := 0; i < 10; i++ {
+		key := fmt.Sprintf("key-%d", i)
+		data := []byte("some data")
+		if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+			t.Error(err)
+		}
+
+		if ok := s.Has(key); !ok {
+			t.Errorf("expected %s to exist", key)
+		}
+
+		r, err := s.Read(key)
+		if err != nil {
+			t.Error(err)
+		}
+		b, err := io.ReadAll(r)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if !bytes.Equal(b, data) {
+			t.Errorf("expected %s, got %s", string(data), string(b))
+		}
+
+		if err := s.Delete(key); err != nil {
+			t.Error(err)
+		}
+
+		if ok := s.Has(key); ok {
+			t.Errorf("expected %s to not exist", key)
+		}
+
+	}
+}
+
+func newStoreHelper() *Store {
 	cfg := StoreConfig{
 		PathTransformFunc: CASPathTransformFunc,
 	}
-	s := NewStore(cfg)
-	key := "special"
+	return NewStore(cfg)
+}
 
-	data := []byte("some data")
-	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
-		t.Error(err)
-	}
-
-	if ok := s.Has(key); !ok {
-		t.Errorf("expected %s to exist", key)
-	}
-
-	r, err := s.Read(key)
-	if err != nil {
-		t.Error(err)
-	}
-	b, err := io.ReadAll(r)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if !bytes.Equal(b, data) {
-		t.Errorf("expected %s, got %s", string(data), string(b))
-	}
-	fmt.Println("SUCCESS READ: " + string(b))
-
-	if err = s.Delete(key); err != nil {
+func teardown(t *testing.T, s *Store) {
+	if err := s.Clear(); err != nil {
 		t.Error(err)
 	}
 }
